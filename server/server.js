6eb2a35d0b597d2ad9278ef09076f3f34b697730
Rachel Hoey <rhoey@hotmail.com>
2021-10-05 20:34:53 -0500

    basic server setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+//requires
+const express = require ('express');
+const app = express ();
+
+const bodyParser = require ('body-parser');
+const port = 5000;
+
+app.use( express.static( 'server/public' ) );
+// NEEDED for POSTS to work
+app.use( bodyParser.urlencoded( { extended: true }))
+
+app.listen( port, ()=>{
+    console.log( 'server up on', port );
+})
