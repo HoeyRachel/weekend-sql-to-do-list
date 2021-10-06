8c24b2c414f2e46e5ac4945957e420fc215f3f0c
Rachel Hoey <rhoey@hotmail.com>
2021-10-05 21:24:55 -0500

    get call working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,23 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log ('in onReady');
+    taskReader();
+}
+
+function taskReader(){
+    console.log ('in taskReader');
+    //Ajax call to get tasks
+    $.ajax({
+        method: 'GET',
+        url: '/newTasks'
+    }).then (function(response){
+        console.log ('back from server succesfully:', response);
+        // let el = $(tasksOut);
+        // el.empty();
+
+    }).catch( function( err ){
+        // got an error
+        alert( 'task entry error' );
+        console.log( 'error:', err );
+})
 }
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express ();
 const bodyParser = require ('body-parser');
 const port = 5000;
 
+let taskList = [];
 app.use( express.static( 'server/public' ) );
 // NEEDED for POSTS to work
 app.use( bodyParser.urlencoded( { extended: true }))
@@ -13,3 +14,7 @@ app.listen( port, ()=>{
     console.log( 'server up on', port );
 })
 
+app.get ('/newTasks', (req,res)=>{
+    console.log ('/newTasks GET hit');
+    res.send (taskList);
+  })
