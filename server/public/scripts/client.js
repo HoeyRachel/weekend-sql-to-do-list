ef686a38e8cf3cc62e701dea552b6e01c05ed6d5
Rachel Hoey <rhoey@hotmail.com>
2021-10-08 21:54:13 -0500

    delete functions written

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,23 @@ $( document ).ready( onReady );
 function onReady(){
     console.log ('in onReady');
     $('#addTaskButton').on('click', taskAdder);
+    $( '#tasksOut' ).on( 'click', '.deleteTaskButton', deleteTask );
+    $( '#tasksOut' ).on( 'click', '.flagTaskButton', flagTask );
     taskReader();
+}
+
+function flagTask(){
+    console.log( 'in flagTask:', $( this ).data( 'id' ) );
+    $.ajax({
+        method: 'PUT',
+        url: '/tasks?id=' + $( this ).data( 'id' ),
+    }).then( function( response ){
+        console.log( 'back from update:', response );
+        getMessages();
+    }).catch( function( err ){
+        console.log( err );
+        alert( 'error flagging task' );
+    })
 }
 
 function taskReader(){
@@ -17,12 +33,16 @@ function taskReader(){
         let el = $(tasksOut);
         el.empty();
         for (let i=0; i<response.length; i++) {
-            el.append (`<li> ${response[i].name} 
+            let appendString = `<li> ${response[i].name} 
                             ${response[i].task}
-                             ${response[i].complete}
-                            </li>`)
+                             ${response[i].complete}`
+                             if( !response[i].flagged ){
+                                appendString += ` <button class="flagTaskButton" data-id="${ response[i].id }">flag</button>`;
+                            }
+                            appendString += `<button class="deleteTaskButton" data-id="${ response[i].id }">delete</button>
+                            </li>`;
+                            el.append( appendString );
         }
-
     }).catch( function( err ){
         // got an error
         alert( 'task entry error' );
@@ -48,5 +68,19 @@ function taskAdder(){
     }).catch (function (err){
         alert ('problem adding task');
         console.log (err);
+    })
+}
+
+function deleteTask(){
+    console.log( 'in deleteTask:', $( this ).data( 'id' ) );
+    $.ajax({
+        method: 'DELETE',
+        url: '/tasks?id=' + $( this ).data( 'id' ),
+    }).then( function( response ){
+        console.log( 'back from delete:', response );
+        getMessages();
+    }).catch( function( err ){
+        console.log( err );
+        alert( 'error deleting task' );
     })
 }
